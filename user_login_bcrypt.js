fb8071614861ac0a6759419dd7ddf62f3f04a08a
Narendra Tanniru <69513865+tannirunarendra@users.noreply.github.com>
2025-01-05 16:20:03 +0530

    Update user_login_bcrypt.js

--- user_login_bcrypt.js
+++ user_login_bcrypt.js
@@ -1,47 +1,49 @@
-const express = require('express')
-const bcrypt = require('bcrypt')
-const userList = require('./model/user.json')
-const path = require('path')
-const app = express()
-app.use(express.json())
+const express = require("express");
+const bcrypt = require("bcrypt");
+const app = express();
+app.use(express.json());
+const userList = [];
+console.log(userList);
+app.post("/user/register/page", async (req, res, next) => {
+  if (!req.body.username || !req.body.password) {
+    return res
+      .status(400)
+      .json({ message: "username and password are required" });
+  }
+  if (userList.find((user) => user.username === req.body.username)) {
+    return res.status(409).json("user already exists");
+  }
+  try {
+    const password = req.body.password;
+    const hashedPass = await bcrypt.hash(password, 10);
+    const new_user = {
+      username: req.body.username,
+      password: hashedPass,
+    };
+    userList.push(new_user);
+    res.status(201).json({ message: "registration successful" });
+  } catch (error) {
+    res.status(500).json({ message: "Error while registering" });
+  }
+});
 
-app.post("/users/register", async (req,res,next)=>{
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password,10);
-        const new_user = {"username":req.body.username,"password":hashedPassword}
-        userList.push(new_user)
-        console.log(userList)
-        res.json({"message":"User added Sussessfully"})
-    } catch (error) {
-        res.status(500).send("failed to add user")
+app.post("/user/login/page", async (req, res) => {
+  const user = userList.find((user) => user.username === req.body.username);
+  if (user == null) {
+    return res.status(400).json({ message: "User not found" });
+  }
+  console.log(userList);
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.status(201).json({ message: "Login Successfull" });
+    } else {
+      res.status(400).json({ message: "please enter a valid password" });
     }
-    
-})
+  } catch (error) {
+    res.status(500).json({ message: "error while logging in" });
+  }
+});
 
-
-app.post("/users/login",async (req,res)=>{
-   
-    const user = userList.find(user=>user.username === req.body.username);
-
-    if(user == null){
-        return res.status(400).json({"message":"Can't find user"})
-    }
-    console.log(user)
-
-    try {
-        
-        if(await bcrypt.compare(req.body.password,user.password)){
-            console.log("login success")
-            res.json({"message":"login success"})
-        }else{
-            console.log("invalid password")
-            res.json({"message":"invalid password"})
-        }
-   } catch (error) {
-    res.status(500).send("error occured")
-   }
-})
-
-
-// console.log(user)
-app.listen(3000,()=>{console.log("server listening on port :", 3000)})
+app.listen(3050, () => {
+  console.log("Server listening on port: ", 3050);
+});
